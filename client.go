6537afa6a56bad84b67515ae560a3b5d37c4ef88
DYJH <953068028@qq.com>
2024-01-16 10:42:12 +0800

    first add

---
+++ client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/rpc/jsonrpc"
+)
+
+func main() {
+	client, err := jsonrpc.Dial("tcp", "127.0.0.1:1234")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer client.Close()
+
+	var request = "Greetings"
+	var reply string
+
+	err = client.Call("HelloService.SayHello", request, &reply)
+	if err != nil {
+		fmt.Println("Error calling remote procedure:", err)
+		return
+	}
+
+	fmt.Println(reply)
+}
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+
+	consul "github.com/hashicorp/consul/api"
+)
+
+type HelloService struct{}
+
+func (h *HelloService) SayHello(request string, reply *string) error {
+	// 获取服务器IP地址
+	ip, err := getServerIP()
+	if err != nil {
+		return err
+	}
+
+	*reply = fmt.Sprintf("Hello world from %s!", ip)
+	return nil
+}
+
+func getServerIP() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					return ipnet.IP.String(), nil
+				}
+			}
+		}
+	}
+
+	return "", fmt.Errorf("Unable to determine server IP address")
+}
+
+func registerServiceWithConsul() error {
+	config := consul.DefaultConfig()
+	client, err := consul.NewClient(config)
+	if err != nil {
+		return err
+	}
+
+	registration := new(consul.AgentServiceRegistration)
+	registration.ID = "hello-service"
+	registration.Name = "hello-service"
+	registration.Port = 1234
+
+	ip, err := getServerIP()
+	if err != nil {
+		return err
+	}
+
+	registration.Address = ip
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	err := registerServiceWithConsul()
+	if err != nil {
+		fmt.Println("Error registering service with Consul:", err)
+		return
+	}
+
+	helloService := new(HelloService)
+	rpc.Register(helloService)
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server registered with Consul and listening on :1234...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
+}
